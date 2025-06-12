d900af96988ef9786c5b1fbdc5169a569cb41a28
Dilip <162571689+dilip2k04@users.noreply.github.com>
2025-06-12 22:28:39 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,16 +6,16 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://chatify-backend-sh82.onrender.com',
         changeOrigin: true,
         secure: false,
       },
       '/socket.io': {
-        target: 'http://localhost:5000',
+        target: 'https://chatify-backend-sh82.onrender.com',
         ws: true,
       },
       '/uploads': {
-        target: 'http://localhost:5000',
+        target: 'https://chatify-backend-sh82.onrender.com',
         changeOrigin: true,
         secure: false,
       }
